0e0be07369b0f2333cf0b5ad11033bf97d1f02f7
thiago-anjos <thiago.w3c@gmail.com>
2020-02-25 19:57:22 -0300

    Updates

---
+++ precache-manifest.5fa67484e06a78dad2cb8ce64ceb52b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb776a2e0db98c930929d1cbc53d683",
+    "url": "/react-nutri-api/index.html"
+  },
+  {
+    "revision": "943b9820b3156b543006",
+    "url": "/react-nutri-api/static/css/main.97524ec8.chunk.css"
+  },
+  {
+    "revision": "0ae98968734b23ae1fb3",
+    "url": "/react-nutri-api/static/js/2.7ee94cb7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-nutri-api/static/js/2.7ee94cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943b9820b3156b543006",
+    "url": "/react-nutri-api/static/js/main.5563c844.chunk.js"
+  },
+  {
+    "revision": "0ff7012ad92184d28a01",
+    "url": "/react-nutri-api/static/js/runtime-main.80763de6.js"
+  }
+]);

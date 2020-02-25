d895a1993ac204442dfcff330c0c53bf6c98afa0
thiago-anjos <thiago.w3c@gmail.com>
2020-02-25 19:58:10 -0300

    send files nutri api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nutri: []
+    }
+  }
+
+  componentDidMount(){
+    let state = this.state;
+    let url = "http://www.sujeitoprogramador.com/rn-api/?api=posts";
+    fetch(url)
+      .then(
+        (response) =>{
+          if(response.status !== 200){
+            console.log('error')
+            return;
+          }
+          response.json().then((data)=>{
+            console.log(data);
+            state.nutri = data;
+            this.setState(state);
+          }) 
+        }
+      )
+      .catch((err)=>{
+        console.log('Erro ao fetch')
+      })
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <header><strong>React Nutri API</strong></header>
+        {this.state.nutri.map((item) => {
+          return(
+            <article key={item.id} className="post">
+                <strong className="titulo">{item.titulo}</strong>
+                <img src={item.capa} className="capa" alt={item.categoria}/>
+                <p className="subtitulo">{item.subtitulo}</p>
+                <button className="botao" href="#">Acessar</button>
+            </article>
+          );
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
